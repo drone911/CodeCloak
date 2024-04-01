3c1e7711648e37417487b4583eb2700de78191c0
Jigar Patel <jigar1822@gmail.com>
2024-04-01 16:41:16 -0400

    Add express limiter

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,8 @@ import { Request, Response, NextFunction } from 'express';
 
 import * as cors from "cors";
 import Helmet from "helmet";
+import { rateLimit } from 'express-rate-limit'
+
 import detectPageRoutes from './routes/detectPageRoutes';
 import landingPageRoutes from './routes/landingPageRoutes';
 import scanPageRoutes from './routes/scanPageRoute'
@@ -10,10 +12,18 @@ import scanPageRoutes from './routes/scanPageRoute'
 
 const app = express.default();
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	limit: 100,
+	standardHeaders: 'draft-7',
+	legacyHeaders: false
+})
+
 const corsOptions = {
     origin: process.env.REACT_CORS_ORIGIN,
 };
 
+app.use(limiter)
 app.use(express.json());
 app.use(cors.default(corsOptions));
 app.use(Helmet());
